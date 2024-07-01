342b9e18972050d538e6ff8cb1ab405e44281803
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-01 17:53:45 +0300

    write remove-all script

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+// npm run remove-all
+
+export const removeAllContacts = async () => {
+  try {
+    fs.writeFile(PATH_DB, JSON.stringify([]), 'utf8');
+    console.log('Succefully removed all contacts');
+  } catch (error) {
+    console.error('Error deleting all contacts:', error);
+  }
+};
 
 removeAllContacts();
