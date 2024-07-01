8d18a895722a479508873e612fe134c3c487f9ef
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-01 16:26:31 +0300

    do task 2 - create add-one script

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  const newContact = createFakeContact();
+  console.log(newContact);
+  try {
+    const fileContent = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(fileContent);
+    contacts.push(newContact);
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts), 'utf8');
+    console.log('Successfully added 1 contact');
+  } catch (error) {
+    console.error('Error adding contact:', error);
+  }
+};
 
 addOneContact();
